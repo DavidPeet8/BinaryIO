eac0fa5e8837af7a027311520f5b4b72d427c128
davipeet8 <davidpeetp@gmail.com>
2020-11-15 20:46:56 -0500

    Updated gitignore

--- build/CMakeFiles/bmanip.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for bmanip.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/david/Programming/Projects/binary_reader
-# Build directory: /home/david/Programming/Projects/binary_reader/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
